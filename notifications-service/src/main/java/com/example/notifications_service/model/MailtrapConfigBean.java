cbcdc82bd101bf12c2070dafe558360477939bd6
Jeferson Rosales <jeferson24j@gmail.com>
2025-12-10 09:37:58 -0300

    Modificacion en la implementacion del servicio de correo

---
+++ notifications-service/src/main/java/com/example/notifications_service/model/MailtrapConfigBean.java
@@ -0,0 +1,23 @@
+package com.example.notifications_service.model;
+
+import io.mailtrap.client.MailtrapClient;
+import io.mailtrap.config.MailtrapConfig;
+import io.mailtrap.factory.MailtrapClientFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MailtrapConfigBean {
+    @Value("${mailtrap.api.token}")
+    private String token;
+
+    @Bean
+    public MailtrapClient mailtrapClient() {
+        MailtrapConfig config = new MailtrapConfig.Builder()
+                .token(token)
+                .build();
+
+        return MailtrapClientFactory.createMailtrapClient(config);
+    }
+}
--- notifications-service/src/main/java/com/example/notifications_service/service/NotificationsService.java
+++ notifications-service/src/main/java/com/example/notifications_service/service/NotificationsService.java
@@ -5,25 +5,56 @@ import com.example.notifications_service.model.Notifications;
 import com.example.notifications_service.repository.IApiUser;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.mailtrap.client.MailtrapClient;
+import io.mailtrap.model.request.emails.Address;
+import io.mailtrap.model.request.emails.MailtrapMail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class NotificationsService implements INotificationsService{
-    @Autowired
-    private JavaMailSender javaMailSender;
+    /*@Autowired
+    private JavaMailSender javaMailSender;*/
 
     @Autowired
     private IApiUser apiUser;
+
+    @Autowired
+    private MailtrapClient mailtrapClient;
+
+    @Value("${mailtrap.sender.email}")
+    private String senderEmail;
+
+    @Value("${mailtrap.sender.name}")
+    private String senderName;
+
 
     @Override
     public void sendNotification(Notifications notifications) {
         UserDTO userDTO = getUserById(notifications.getIdUser());
 
+        MailtrapMail mail = MailtrapMail.builder()
+                .from(new Address(senderEmail, senderName))
+                // Aqui para que el correo se envia a cada usuario se debe setear en new Address "userDTO.getMail()"
+                .to(List.of(new Address("jeferson24j@gmail.com")))
+                .subject(notifications.getSubject())
+                .text("Hola " + userDTO.getName()+ " " + notifications.getText())
+                .category("Notification")
+                .build();
+
+        try {
+            mailtrapClient.send(mail);
+        } catch (Exception e) {
+            System.err.println("Error enviando email: " + e);
+        }
+
+        /*
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("hello@demomailtrap.com");
         // Aqui para que el correo se envia a cada usuario se debe setear en setTo "userDTO.getMail()"
@@ -31,6 +62,7 @@ public class NotificationsService implements INotificationsService{
         message.setSubject(notifications.getSubject());
         message.setText("Hola " + userDTO.getName()+ " " + notifications.getText());
         javaMailSender.send(message);
+        */
     }
 
     @CircuitBreaker(name = "users-service", fallbackMethod = "fallbackMethodGetUserById")
